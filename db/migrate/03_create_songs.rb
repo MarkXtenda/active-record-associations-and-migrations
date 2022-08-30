034b32a9378d1c40369b4c4c13150cf8a40a3df7
MarkXtenda <mark.i2p.1998@gmail.com>
2022-08-30 13:47:35 +1000

    done

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,2 +1,9 @@
 class CreateSongs < ActiveRecord::Migration[4.2]
+    def change
+        create_table :songs do |r|
+            r.string :name
+            r.integer :artist_id
+            r.integer :genre_id
+        end
+    end
 end
